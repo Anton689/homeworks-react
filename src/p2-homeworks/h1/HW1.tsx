8e475c391752cb8ee3867368a57f359f7fddfcdd
Anton689 <zizugum@gmail.com>
2021-12-10 18:09:49 +0300

    added solution HW1

---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Message from "./Message";
+
+type MessageDataType = {
+    avatar: string;
+    name: string;
+    message: string;
+    time: string;
+}
+
+const messageData: MessageDataType = {
+    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+    name: 'Anton',
+    message: 'Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello',
+    time: '22:00',
+}
+
+
+function HW1() {
+
+    return (
+        <div>
+            <hr/>
+            homeworks 1
+            {/*should work (должно работать)*/}
+            <Message avatar={messageData.avatar}
+                     name={messageData.name}
+                     text={messageData.message}
+                     time={messageData.time}/>
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeMessage/>*/}
+            <hr/>
+        </div>
+    )
+}
+
+export default HW1
---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import s from './Message.module.css';
+
+type propsType = {
+    avatar: string;
+    name: string;
+    text: string;
+    time: string;
+}
+
+function Message(props: propsType) {
+    return (
+        <div className={s.dialogWindow}>
+            {/*<div className={s.avatar}>*/}
+            {/*    <img src={props.avatar} alt=""/>*/}
+            {/*</div>*/}
+            {/*<div className={s.message}>*/}
+            {/*    <div className={s.text}>*/}
+            {/*        {props.text}*/}
+            {/*    </div>*/}
+            {/*    <div className={s.name}>*/}
+            {/*        {props.name}*/}
+            {/*    </div>*/}
+            {/*    <div className={s.time}>*/}
+            {/*        {props.time}*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            <div className={s.div}></div>
+            <div className={s.div2}></div>
+
+        </div>
+
+    )
+}
+
+export default Message
