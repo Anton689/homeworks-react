ec9d36d79c5aa1c6a853bbd17a184f99db8ebc48
Anton689 <zizugum@gmail.com>
2021-12-13 23:21:53 +0300

    updated CSS

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -11,29 +11,20 @@ type propsType = {
 function Message(props: propsType) {
     return (
         <div className={s.dialogWindow}>
-            <div className={s.avatar}>
-                <img src={props.avatar} alt=""/>
-            </div>
+            <img src={props.avatar} alt="" className={s.avatar}/>
+            <div className={s.div}></div>
             <div className={s.message}>
-                <div className={s.text}>
-                    {props.text}
-                </div>
                 <div className={s.name}>
                     {props.name}
+                </div>
+                <div className={s.text}>
+                    {props.text}
                 </div>
                 <div className={s.time}>
                     {props.time}
                 </div>
-
             </div>
-            {/*<div className={s.div}>*/}
-            {/*</div>*/}
-
-
-
-
         </div>
-
     )
 }
 
