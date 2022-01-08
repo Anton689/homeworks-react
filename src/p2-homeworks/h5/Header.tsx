6bd0f68c2df93c53a41f87ad51098d88409f1072
Anton689 <zizugum@gmail.com>
2022-01-08 21:06:58 +0300

    added HW5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import {PATH} from './RouteComp';
+import s from './Style.module.css';
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={s.header}>
+            <span> <NavLink to={PATH.PRE_JUNIOR} className={({isActive}) => isActive ? s.active : ''}>PreJunior</NavLink> </span>
+            <span> <NavLink to={PATH.JUNIOR} className={({isActive}) => isActive ? s.active : ''}>Junior</NavLink> </span>
+            <span> <NavLink to={PATH.JUNIOR_PLUS} className={({isActive}) => isActive ? s.active : ''}>Junior Plus</NavLink></span>
+            <span> <NavLink to={'/error'} className={({isActive}) => isActive ? s.active : ''}>Error</NavLink></span>
 
         </div>
     )
